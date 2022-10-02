51b1aafc9da82efe3691f8eeb2e10baebfc761df
Morion <adrian.martinez@udea.edu.co>
2022-10-02 18:33:33 -0500

    Nuevo intento por arreglar

--- Assets/Scripts/SonidoTimon.cs
+++ Assets/Scripts/SonidoTimon.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class SonidoTimon : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform timon;
+    public float velocidad;
     void Start()
     {
         
